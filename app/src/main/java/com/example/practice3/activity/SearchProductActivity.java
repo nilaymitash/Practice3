fd18a3d0f2c8e9b42384b24aa30ed49b36002629
N Mitash <63482714+nilaymitash@users.noreply.github.com>
2023-04-11 13:48:45 -0400

    Added Recycler View

--- app/src/main/java/com/example/practice3/activity/SearchProductActivity.java
+++ app/src/main/java/com/example/practice3/activity/SearchProductActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.practice3.R;
 import com.example.practice3.config.AppDatabaseHelper;
@@ -23,7 +24,7 @@ import java.util.List;
 
 public class SearchProductActivity extends AppCompatActivity {
 
-    private ListView mProductListView;
+    private RecyclerView mProductRecyclerView;
     private TextView mLogoutLink;
     private AppDatabaseHelper databaseHelper;
 
@@ -33,7 +34,7 @@ public class SearchProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_product);
 
         mLogoutLink = findViewById(R.id.logout_link);
-        mProductListView = findViewById(R.id.product_listview);
+        mProductRecyclerView = findViewById(R.id.product_recyclerView);
 
         databaseHelper = new AppDatabaseHelper(this);
         ProductMaintenanceDao dao = new ProductMaintenanceDao(databaseHelper);
@@ -51,9 +52,13 @@ public class SearchProductActivity extends AppCompatActivity {
             dao.populateDummyProducts(SearchProductActivity.this);
         }
 
-        ProductListAdapter adapter = new ProductListAdapter(SearchProductActivity.this, dao.getAllProducts());
-        mProductListView.setAdapter(adapter);
-        mProductListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        List<Product> products = dao.getAllProducts();
+
+        ProductListAdapter adapter = new ProductListAdapter(products);
+        mProductRecyclerView.setAdapter(adapter);
+
+
+        /*mProductRecyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Product selectedProduct = (Product) adapterView.getItemAtPosition(position);
@@ -62,7 +67,7 @@ public class SearchProductActivity extends AppCompatActivity {
                 //TODO: add search filter/sort criteria as extra
                 startActivity(intent);
             }
-        });
+        });*/
     }
 
     @Override
@@ -76,53 +81,5 @@ public class SearchProductActivity extends AppCompatActivity {
         }
     }
 
-    /*private ArrayList<Product> populateDemoProductList() {
-        ArrayList<Product> productList = new ArrayList<>();
 
-        Product p1 = new Product();
-        p1.setTitle(getResources().getString(R.string.wings_name));
-        p1.setPrice(new BigDecimal("4.69"));
-        p1.setDescription(getResources().getString(R.string.wings_description));
-        p1.setImgSrc(R.drawable.wings);
-        p1.setFeedback(populateFeedbackList(114, 3.5F));
-
-        Product p2 = new Product();
-        p2.setTitle(getResources().getString(R.string.pb_name));
-        p2.setPrice(new BigDecimal("4.49"));
-        p2.setDescription(getResources().getString(R.string.pb_description));
-        p2.setImgSrc(R.drawable.pb);
-        p2.setFeedback(populateFeedbackList(76, 4.5F));
-
-        Product p3 = new Product();
-        p3.setTitle(getResources().getString(R.string.pickled_pups_name));
-        p3.setPrice(new BigDecimal("4.99"));
-        p3.setDescription(getResources().getString(R.string.pickled_pups_description));
-        p3.setImgSrc(R.drawable.pups);
-        p3.setFeedback(populateFeedbackList(34, 5.0F));
-
-        Product p4 = new Product();
-        p4.setTitle(getResources().getString(R.string.sb_preserve_name));
-        p4.setPrice(new BigDecimal("3.69"));
-        p4.setDescription(getResources().getString(R.string.sb_preserve_description));
-        p4.setImgSrc(R.drawable.strawberry);
-        p4.setFeedback(populateFeedbackList(1123, 4.0F));
-
-        productList.add(p1);
-        productList.add(p2);
-        productList.add(p3);
-        productList.add(p4);
-        return productList;
-    }*/
-
-    private List<Feedback> populateFeedbackList(int numOfRatings, float avgRating) {
-        List<Feedback> list = new ArrayList<>();
-
-        for(int i = 0; i < numOfRatings; i++) {
-            Feedback feedback = new Feedback();
-            feedback.setRating(avgRating);
-            list.add(feedback);
-        }
-
-        return list;
-    }
 }
--- app/src/main/java/com/example/practice3/model/ProductListAdapter.java
+++ app/src/main/java/com/example/practice3/model/ProductListAdapter.java
@@ -1,55 +1,72 @@
 package com.example.practice3.model;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.practice3.R;
 import com.example.practice3.util.Utility;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class ProductListAdapter extends ArrayAdapter<Product> {
+public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.ViewHolder> {
 
-    public ProductListAdapter(@NonNull Context context, @NonNull ArrayList<Product> products) {
-        super(context, R.layout.product_list_item, products);
+    private List<Product> products;
+
+    public ProductListAdapter(List<Product> products) {
+        this.products = products;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if(convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.product_list_item, parent, false);
-        }
-
-        Product product = getItem(position);
-
-        TextView mTextViewName = convertView.findViewById(R.id.product_title);
-        TextView mTextViewPrice = convertView.findViewById(R.id.product_price);
-        TextView mTextViewDescription = convertView.findViewById(R.id.product_description);
-        ImageView mProductImage = convertView.findViewById(R.id.product_img_holder);
-        RatingBar mProductRatingBar = convertView.findViewById(R.id.product_rating_bar);
-        TextView mProductRating = convertView.findViewById(R.id.product_rating);
-        TextView mNumOfRatings = convertView.findViewById(R.id.num_of_ratings);
-
-        mProductImage.setImageResource(product.getImgSrc());
-        mTextViewName.setText(String.valueOf(product.getTitle()));
-        mTextViewPrice.setText(Utility.formatCurrency(product.getPrice()));
-        mTextViewDescription.setText(Utility.truncateString(product.getDescription(), 160));
-        mProductRating.setText(String.valueOf(product.getRating()));
-        mProductRatingBar.setRating(product.getRating());
-        mNumOfRatings.setText("(" + product.getNumOfRatings() + ")");
-
-        return convertView;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_list_item,
+                parent, false);
+        return new ViewHolder(view);
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Product product = products.get(position);
 
+        holder.mProductImage.setImageResource(product.getImgSrc());
+        holder.mTextViewName.setText(String.valueOf(product.getTitle()));
+        holder.mTextViewPrice.setText(Utility.formatCurrency(product.getPrice()));
+        holder.mTextViewDescription.setText(Utility.truncateString(product.getDescription(), 160));
+        holder.mProductRating.setText(String.valueOf(product.getRating()));
+        holder.mProductRatingBar.setRating(product.getRating());
+        holder.mNumOfRatings.setText("(" + product.getNumOfRatings() + ")");
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewName;
+        public TextView mTextViewPrice;
+        public TextView mTextViewDescription;
+        public ImageView mProductImage;
+        public RatingBar mProductRatingBar;
+        public TextView mProductRating;
+        public TextView mNumOfRatings;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextViewName = itemView.findViewById(R.id.product_title);
+            mTextViewPrice = itemView.findViewById(R.id.product_price);
+            mTextViewDescription = itemView.findViewById(R.id.product_description);
+            mProductImage = itemView.findViewById(R.id.product_img_holder);
+            mProductRatingBar = itemView.findViewById(R.id.product_rating_bar);
+            mProductRating = itemView.findViewById(R.id.product_rating);
+            mNumOfRatings = itemView.findViewById(R.id.num_of_ratings);
+        }
+    }
 }
